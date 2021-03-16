efc516109a26ccafca696b6df0a4114fd8864f5f
hajdukka <nerminhajdo@gmail.com>
2021-03-16 23:03:01 +0100

    Dodao ex11-13

---
+++ ex13/src/index.js
@@ -0,0 +1,18 @@
+function inverseWhile(){
+
+    var fiveNumbers = "";
+    var i = 5;
+
+    while(i>=0){
+        fiveNumbers += i--;
+        if(i>=0){
+            fiveNumbers += ",";
+        }
+    }
+
+    return fiveNumbers;
+
+}
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
